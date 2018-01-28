f0543faf80fec5c5340c55975b5afa73b266084c
Dhinojwala <419091@AMB00683.local>
2018-01-28 10:25:27 +0530

    adding ci folder

---
+++ ci/tasks/smoke-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt-get update && apt-get install -y curl
+set -ex
+
+if [ -z $MOVIE_FUN_URL ]; then
+  echo "MOVIE_FUN_URL not set"
+  exit 1
+fi
+
+pushd apps-movie-fun-code-spring-boot
+  echo "Running smoke tests for Movie Fun deployed at $MOVIE_FUN_URL"
+  smoke-tests/bin/test $MOVIE_FUN_URL
+popd
+
+exit 0
