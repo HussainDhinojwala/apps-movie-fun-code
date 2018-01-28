ae5e34509ba53ce992aad55dc64e904e77ce2664
Dhinojwala <419091@AMB00683.local>
2018-01-28 10:50:47 +0530

    adding jar file changes

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,19 +2,19 @@
 
 set -e +x
 
-pushd attendee-service-source
+pushd apps-movie-fun-code-spring-boot
   echo "Packaging JAR"
   ./mvnw clean package -DskipTests
 popd
 
-jar_count=`find attendee-service-source/target -type f -name *.jar | wc -l`
+jar_count=`find apps-movie-fun-code-spring-boot/target -type f -name *.jar | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find attendee-service-source/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
+find apps-movie-fun-code-spring-boot/target -type f -name *.jar -exec cp "{}" package-output/movie-fun-code.jar \;
 
 echo "Done packaging"
 exit 0
